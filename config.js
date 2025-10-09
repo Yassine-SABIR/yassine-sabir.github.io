165ade21ed64ed13d3bc8babbaf3496896ae69a2
Yassine-SABIR <sabiryassine001@gmail.com>
2025-10-09 22:33:28 +0200

    Supprime .gitignore et ajoute tous les fichiers

---
+++ config.js
@@ -0,0 +1,5 @@
+// Duplicate this file as config.js and replace the placeholder value with your CORS proxy key.
+window.__APP_CONFIG__ = {
+  corsProxyKey: 'temp_afdb76935e838222628f658f88baf653',
+  contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbzahbbFxd_XzSm0VgURBj11cj9nBu1SFrBE236VGt5PRU0XviLOx4hmKl_dTYpKh5lc/exec' // Replace with your Google Apps Script Web App URL
+};
