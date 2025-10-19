14f86282e5506fc61907e3e47544ed2a3b8dec50
Yassine SABIR <82727031+Yassine-SABIR@users.noreply.github.com>
2025-10-19 23:28:36 +0200

    Change contactSheetEndpoint in config.js
    Updated the contactSheetEndpoint URL for the application.

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// Duplicate this file as config.js and replace the placeholder value with your CORS proxy key.
 window.__APP_CONFIG__ = {
   corsProxyKey: 'temp_afdb76935e838222628f658f88baf653',
-  contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbzahbbFxd_XzSm0VgURBj11cj9nBu1SFrBE236VGt5PRU0XviLOx4hmKl_dTYpKh5lc/exec' // Replace with your Google Apps Script Web App URL
+  contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbyxGAz-6JzOYkiij98uN4gYAl32cDsV3ijCR122VHGC00YYRVcH5_55ppmHrtjNiHJL/exec' // Replace with your Google Apps Script Web App URL
 };
