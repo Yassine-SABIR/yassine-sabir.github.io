5b625c375952b86f8d74120fcf494c33948722d9
Yassine-SABIR <sabiryassine001@gmail.com>
2026-04-20 09:43:33 +0200

    -

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 window.__APP_CONFIG__ = {
   corsProxyKey: 'temp_afdb76935e838222628f658f88baf653',
-  contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbyxGAz-6JzOYkiij98uN4gYAl32cDsV3ijCR122VHGC00YYRVcH5_55ppmHrtjNiHJL/exec' //Google Apps Script Web App URL
-  
+  contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbyxGAz-6JzOYkiij98uN4gYAl32cDsV3ijCR122VHGC00YYRVcH5_55ppmHrtjNiHJL/exec', //Google Apps Script Web App URL
   corsProxyKey: 'temp_afdb76935e838222628f658f88baf653', //Just a temp key
   contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbyxGAz-6JzOYkiij98uN4gYAl32cDsV3ijCR122VHGC00YYRVcH5_55ppmHrtjNiHJL/exec'
 };
