51da62d323f7b9cef99f54dd48705bf70ebbdbbe
Yassine-SABIR <sabiryassine001@gmail.com>
2026-05-09 20:24:37 +0200

    Update main files

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
 window.__APP_CONFIG__ = {
-<<<<<<< HEAD
-  corsProxyKey: 'temp_afdb76935e838222628f658f88baf653',
-  contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbyxGAz-6JzOYkiij98uN4gYAl32cDsV3ijCR122VHGC00YYRVcH5_55ppmHrtjNiHJL/exec' // Replace with your Google Apps Script Web App URL
-=======
-  corsProxyKey: 'temp_afdb76935e838222628f658f88baf653', //Just a temp key
-  contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbyxGAz-6JzOYkiij98uN4gYAl32cDsV3ijCR122VHGC00YYRVcH5_55ppmHrtjNiHJL/exec'
->>>>>>> 56ad5c3 (Writeups)
+  corsProxyKey: 'temp_afdb76935e838222628f658f88baf653',  //Just a temp key
+  contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbxVSTp_FyKs_jZFF9mTkOCFo9Dgw5iUDFoQKT0E9kuYjgtdn0dNfXFrzea7N33Exqfn/exec' //Google Apps Script Web App URL
 };
