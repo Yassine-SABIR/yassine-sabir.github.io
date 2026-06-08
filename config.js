765b9c5411c50d63b052f41acbbccc6c23fae504
Yassine-SABIR <sabiryassine001@gmail.com>
2026-06-08 02:16:15 +0200

    Update HTB stats display mechanism

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.__APP_CONFIG__ = {
-  corsProxyKey: 'temp_afdb76935e838222628f658f88baf653',  //Just a temp key
   contactSheetEndpoint: 'https://script.google.com/macros/s/AKfycbxVSTp_FyKs_jZFF9mTkOCFo9Dgw5iUDFoQKT0E9kuYjgtdn0dNfXFrzea7N33Exqfn/exec' //Google Apps Script Web App URL
 };
